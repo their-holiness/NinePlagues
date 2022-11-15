3752761d7a3881917b0fff2c96a5a10c03d21528
Immanuelle <49389064+their-holiness@users.noreply.github.com>
2022-11-15 07:54:11 -0800

    Create Prescription.js
    File for descriptions of what to do for your plagues

---
+++ Prescription.js
@@ -0,0 +1,19 @@
+Domicide
+
+Your problems mostly center around disconnection from your environment
+
+You should work on getting involved with the church community
+
+Regular activities, readings, and close relationships will help you thrive
+
+Foolishness
+
+Your problems mostly lie in focus and attentional discipline
+
+You should focus on meditations, sports, and activities
+
+Entrapment
+
+Your problems mostly center around not being exposed to new ideas
+
+You should focus on exploring new ideas
