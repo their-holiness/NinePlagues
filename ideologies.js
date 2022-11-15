8533a260f0c2d90bddaa0ba76a2fd33efd18fa06
Immanuelle <49389064+Immanuelle@users.noreply.github.com>
2022-11-14 16:49:03 -0800

    Update ideologies.js

--- ideologies.js
+++ ideologies.js
@@ -1,7 +1,7 @@
 ideologies = [
     {
-        "name": "Plagued",
-		"desc": "An equal level of all plagues",
+        "name": "Extremely Plagued",
+		"desc": "A high level of all plagues",
 		"theory":"I'll be happy if you even read in general.",
         "stats": {
             "alien": 100,
@@ -13,6 +13,86 @@ ideologies = [
 			"foreign": 100,
 			"ego": 100,
 			"empath": 100
+        }
+    },
+    {
+        "name": "Moderately Plagued",
+		"desc": "A moderate level of all plagues",
+		"theory":"I'll be happy if you even read in general.",
+        "stats": {
+            "alien": 50,
+            "angst": 50,
+            "absurd": 50,
+            "forg": 50,
+			"spiral": 50,
+			"drown": 50,
+			"foreign": 50,
+			"ego": 50,
+			"empath": 50
+        }
+    },
+    {
+        "name": "Homeless",
+		"desc": "Plagues center in domicide, you need a supportive community",
+		"theory":"I'll be happy if you even read in general.",
+        "stats": {
+            "alien": 100,
+            "angst": 100,
+            "absurd": 100,
+            "forg": 50,
+			"spiral": 50,
+			"drown": 50,
+			"foreign": 50,
+			"ego": 50,
+			"empath": 50
+        }
+    },
+    {
+        "name": "Foolish",
+		"desc": "Plagues center in foolishness, you need to meditate more",
+		"theory":"I'll be happy if you even read in general.",
+        "stats": {
+            "alien": 50,
+            "angst": 50,
+            "absurd": 50,
+            "forg": 100,
+			"spiral": 100,
+			"drown": 100,
+			"foreign": 50,
+			"ego": 50,
+			"empath": 50
+        }
+    },
+    {
+        "name": "Entrapped",
+		"desc": "Plagues center in entrapment, You need to question your environment more",
+		"theory":"I'll be happy if you even read in general.",
+        "stats": {
+            "alien": 50,
+            "angst": 50,
+            "absurd": 50,
+            "forg": 50,
+			"spiral": 50,
+			"drown": 100,
+			"foreign": 100,
+			"ego": 100,
+			"empath": 100
+        }
+    },
+    {
+        "name": "Noble",
+		"desc": "Liberated from the plagues",
+		"theory":"I'll be happy if you even read in general.",
+        "stats": {
+            "alien": 0,
+            "angst": 0,
+            "absurd": 0,
+            "forg": 0,
+			"spiral": 0,
+			"drown": 0,
+			"foreign": 0,
+			"ego": 0,
+			"empath": 0
         }
     }
 ];
